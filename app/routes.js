c427b2d9364c005718a3d80ba27bef57f3b49d9c
Christopher McLean <havok2905@gmail.com>
2016-03-01 18:42:12 -0500

    routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,29 @@
 var mongoose  = require('mongoose');
-var Foo       = require('./model.js');
+var Job       = require('./model.js');
 
 module.exports = function(app) {
+  app.get('/job', function(request, response) {
+    Job.findById('', function(error, job) {
+      if(error){ response.send(error); }
+      response.json(job);
+    });
+  });
 
+  app.get('/jobs', function(request, response) {
+    var query = Job.find({});
+
+    query.exec(function(error, jobs) {
+      if(error){ response.send(error); }
+      response.json(jobs);
+    });
+  });
+
+  app.post('/jobs', function(request, response) {
+    var newJob = new Job(request.body);
+
+    newJob.save(function(error) {
+      if(error){ response.send(error); }
+      response.json(request.body);
+    })
+  });
 }
