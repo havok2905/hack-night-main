c3eec6cc78ab0fc6086f3701997953d3353f2c89
Christopher McLean <havok2905@gmail.com>
2016-03-01 19:03:44 -0500

    job crud

--- public/js/jobFactory.js
+++ public/js/jobFactory.js
@@ -1,7 +1,46 @@
 app.factory('jobFactory', function($http) {
 
-  return {
+  function jobFromForm(jobData) {
+    return {
+      name:       jobData.name,
+      html:       parseInt(jobData.html),
+      css:        parseInt(jobData.css),
+      javascript: parseInt(jobData.javascript),
+      php:        parseInt(jobData.php),
+      ruby:       parseInt(jobData.ruby),
+      java:       parseInt(jobData.java),
+      c:          parseInt(jobData.c),
+      python:     parseInt(jobData.python)
+    };
+  }
 
+  function resetJobForm(jobData) {
+    jobData.name = '';
+    jobData.html = '';
+    jobData.css = '';
+    jobData.javascript = '';
+    jobData.php = '';
+    jobData.ruby = '';
+    jobData.java = '';
+    jobData.c = '';
+    jobData.python = '';
+
+    return jobData;
+  }
+
+  function getJobs() {
+    return $http.get('/jobs');
+  }
+
+  function postJob(job) {
+    return $http.post('/jobs', job);
+  }
+
+  return {
+    jobFromForm: jobFromForm,
+    resetJobForm: resetJobForm,
+    getJobs: getJobs,
+    postJob: postJob
   };
 
 });
--- public/js/mainController.js
+++ public/js/mainController.js
@@ -1,3 +1,21 @@
 app.controller('mainController', function($scope, githubFactory, jobFactory, comparisonFactory) {
-  $scope.jobs = [];
+
+  jobFactory.getJobs()
+            .success(function(data) { $scope.jobs = data; })
+            .error(function(error) { console.log(error); });
+
+  $scope.createJob = function() {
+    var form = $scope.jobForm;
+    var job  = jobFactory.jobFromForm(form);
+
+    jobFactory.postJob(job)
+              .success(function(data) {
+                jobFactory.resetJobForm(form);
+                $scope.jobs.push(data);
+              })
+              .error(function(data) {
+                console.log(data);
+              });
+  };
+
 });
